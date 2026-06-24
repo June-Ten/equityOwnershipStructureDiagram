6df96cbde666e3601da4fc3a303ec70bd5552ee2
wangqiling <wangqiling@wpooldata.com>
2026-06-24 10:58:19 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const base = repoName ? `/${repoName}/` : '/'
+
 export default defineConfig({
+  base,
   plugins: [vue()],
 })
